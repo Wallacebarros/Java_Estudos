2e5677a9171dc0af6ec301c3e05f74ffc4da487a
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-06-27 10:43:59 -0300

    metodo: adicionar a pilha

--- projetos_praticos/Pilha/src/Pilha.java
+++ projetos_praticos/Pilha/src/Pilha.java
@@ -1,9 +1,15 @@
 public class Pilha {
 
     private int quantidade = 0;
+    private String[] elementos = new String[10];
 
     public boolean vazia() {
         return quantidade == 0;
+    }
+
+    public void adicionar(String elemento) {
+        elementos[quantidade] = elemento;
+        quantidade++;
     }
 
 }
