7e8ea2c2b9c0b87f26640f8d9bf801ffd0c10e3d
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-07-02 12:26:50 -0300

    Implementando a class Livro

---
+++ projetos_praticos/Biblioteca/src/main/java/br/com/estudos_java/biblioteca/classes/Livro.java
@@ -0,0 +1,12 @@
+package br.com.estudos_java.biblioteca.classes;
+
+public class Livro {
+    private String titulo, autor;
+    private int paginas;
+
+    public Livro(String titulo, String autor, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+    }
+}
