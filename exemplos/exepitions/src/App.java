9424137cdd8d8f5fe8f381cd57037fe79164dba5
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-07-03 18:42:01 -0300

    Codigo execultando exeção

--- exemplos/exepitions/src/App.java
+++ exemplos/exepitions/src/App.java
@@ -1,15 +1,16 @@
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         a(100);
         System.out.println("Execultando Main");
     }
     
-    public static void a(int i) {
+    public static void a(int i) throws Exception {
         System.out.println("Execultando a(): " + i);
         b(i);
     }
 
-    public static void b(int i) {
+    public static void b(int i) throws Exception {
         System.out.println("Execultando b(): " + i);
+        throw new Exception("Minha exeção");
     }
 }
