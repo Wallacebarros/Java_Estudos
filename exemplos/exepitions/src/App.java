ff02fd3fcd1d000802f28317188bc5cd25b97737
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-07-03 18:44:58 -0300

    codigo com exeção tratada

--- exemplos/exepitions/src/App.java
+++ exemplos/exepitions/src/App.java
@@ -1,16 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
         a(100);
-        System.out.println("Execultando Main");
+        System.out.println("Execultando Main()");
     }
     
     public static void a(int i) throws Exception {
-        System.out.println("Execultando a(): " + i);
-        b(i);
+        try {
+            System.out.println("Execultando a(): " + i);
+            b(i);
+        } catch (Exception e) {
+            System.out.println("Tratando: " + e.getMessage());
+        }
     }
 
     public static void b(int i) throws Exception {
         System.out.println("Execultando b(): " + i);
-        throw new Exception("Minha exeção");
+        throw new Exception("Minha exeption");
     }
 }
